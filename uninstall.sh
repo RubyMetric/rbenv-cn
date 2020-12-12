2d4618b2bbc05285a5e51da1ae0481e45d890095
ccmywish <ccmywish@qq.com>
2020-12-12 12:45:41 +0800

    del rbenv init

--- uninstall.sh
+++ uninstall.sh
@@ -5,10 +5,12 @@ echo "=> 删除相关环境变量"
 sed -i '/rbenv config/d' "${HOME}/.bashrc"
 sed -i '/.rbenv\/bin:/d' "${HOME}/.bashrc"
 sed -i '/export RUBY_BUILD_MIRROR_URL=https/d' "${HOME}/.bashrc"
+sed -i '/eval \"\$(rbenv init -)\"/d' "${HOME}/.bashrc"
 
 sed -i '/rbenv config/d' "${HOME}/.zshrc"
 sed -i '/.rbenv\/bin:/d' "${HOME}/.zshrc"
 sed -i '/export RUBY_BUILD_MIRROR_URL=https/d' "${HOME}/.zshrc"
+sed -i '/eval \"\$(rbenv init -)\"/d' "${HOME}/.zshrc"
 
 echo "=> 删除rbenv-update"
 sudo rm  /usr/local/bin/rbenv-update
